35b4386cd80456718e790f63eb48dbd2692c67da
alexander.fournier <alexander.fournier@nasm.org>
2018-02-26 12:44:45 -0700

    first commit

---
+++ pluralsight-springcloud-m1-startup/src/main/java/com/example/demo/PluralsightSpringcloudM1StartupApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PluralsightSpringcloudM1StartupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralsightSpringcloudM1StartupApplication.class, args);
+	}
+	
+	public String sayHello() {
+		return "Hello from springboot";
+	}
+}
