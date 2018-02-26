0d165d2212b4c4c44732b82cd92ba83c67d31253
alexander.fournier <alexander.fournier@nasm.org>
2018-02-26 13:53:28 -0700

    second get

--- pluralsight-springcloud-m1-startup/src/main/java/com/example/demo/PluralsightSpringcloudM1StartupApplication.java
+++ pluralsight-springcloud-m1-startup/src/main/java/com/example/demo/PluralsightSpringcloudM1StartupApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,7 +14,16 @@ public class PluralsightSpringcloudM1StartupApplication {
 		SpringApplication.run(PluralsightSpringcloudM1StartupApplication.class, args);
 	}
 	
-	public String sayHello() {
-		return "Hello from springboot";
+
+	@RequestMapping(value="/employee", method=RequestMethod.GET)
+	public String sayEmployee() {
+		return "alex fournier";
+	}
+		
+	@RequestMapping(value="/greeting", method=RequestMethod.GET)
+		public String sayGreeting() {
+		return "hello world";
+	
 	}
 }
+
